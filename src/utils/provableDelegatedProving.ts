3449cb5aba6c35892533bf9e3f21b66fe0bdecd8
shaibriz <97502054+shaibriz@users.noreply.github.com>
2026-02-02 23:51:55 +0545

    Fix programName assignment in provableDelegatedProving

--- src/utils/provableDelegatedProving.ts
+++ src/utils/provableDelegatedProving.ts
@@ -83,7 +83,9 @@ export async function executeWithProvableDelegatedProving({
 
   const proverUrl =
     delegatedConfig.proverUrl ?? defaultProverUrl(leoCliConfig.network);
-  const programName = "veru_oracle_interface_v3.aleo" || oracleConfig.aleoProgram.name;
+  const programName = "veru_oracle_interface_v3.aleo";
+  // const programName = oracleConfig.aleoProgram.name;
+  
   const baseFeeCredits = delegatedConfig.baseFeeCredits ?? 0.25;
   const priorityFeeCredits = delegatedConfig.priorityFeeCredits ?? 0;
   const privateFee = delegatedConfig.privateFee ?? false;
